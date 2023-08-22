dadbef03257b51d9ca4f24b68de8adea8f37ef27
Vadim <vadims.krasovskij01@gmail.com>
2023-08-22 12:13:05 +0300

    Complete rank movie app

--- MovieRating-App/MovieRating-App/ClientApp/src/components/RankItems.js
+++ MovieRating-App/MovieRating-App/ClientApp/src/components/RankItems.js
@@ -1,41 +1,79 @@
-﻿import React, {useState, useEffect} from 'react'
-import MovieImageArr from './MovieImages';
-import RankingGrid from './RankingGrid';
+﻿import {useEffect, useState } from 'react';
+import RankingGrid from "./RankingGrid";
+import ItemCollection from "./ItemCollection";
 
-const RankItems = () => {
+const RankItems = ({items, setItems, dataType, imgArr, localStorageKey }) => {
 
-    const [items, setItems] = useState([]);
-    const dataType = 1;
+    const [reload, setReload] = useState(false);
 
+    function Reload() {
+        setReload(true);
+    }
+
+    function drag(ev) { 
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    function drop(ev) {
+
+        ev.preventDefault();
+        const targetElm = ev.target;
+        if (targetElm.nodeName === "IMG") {
+            return false;
+        }
+        if (targetElm.childNodes.length === 0) {
+            var data = parseInt(ev.dataTransfer.getData("text").substring(5));
+            const transformedCollection = items.map((item) => (item.id === parseInt(data)) ?
+            { ...item, ranking: parseInt(targetElm.id.substring(5)) } : { ...item, ranking: item.ranking });
+            setItems(transformedCollection);
+        }
+
+    }
     useEffect(() => {
+        if (items == null) {
+            getDataFromApi();
+        }
+
+    }, [dataType]);
+
+    function getDataFromApi() {
         fetch(`item/${dataType}`)
             .then((results) => {
-                return results.json()
+                return results.json();
             })
-            .then(data =>  {
+            .then(data => {
+
                 setItems(data);
             })
-    }, [])
+    }
 
-  return (
-    <main>
-        <RankingGrid items={items} imgArr={MovieImageArr}/>
-        <div className="items-not-ranked">
-        {
-            (items.length > 0) ? items.map((item) => 
-            <div className='unranked-cell'>
-                <img id={`item-${item.id}`} src={MovieImageArr.find(el => el.id === item.imageId)?.image}/>        
-            </div>                   
-            ) : <div>Loading....</div>
+    useEffect(() => {
+        if (items != null) {
+            localStorage.setItem(localStorageKey, JSON.stringify(items));
         }
-        
-        </div>
-       
-      
-    </main>
-  )
+        setReload(false);
+    }, [items])
+
+    useEffect(() => {
+        if (reload === true) {
+            getDataFromApi();
+        }
+    },[reload])
+
+
+    return (
+         (items != null)?
+            <main>
+                <RankingGrid items={items} imgArr={imgArr} drag={drag} allowDrop={allowDrop} drop={drop } />
+                <ItemCollection items={items} drag={drag} imgArr={imgArr} />
+                <button onClick={Reload} className="reload" style={{ "marginTop": "10px" }}> <span className="text" >Reload</span > </button>
+            </main>
+            : <main>Loading...</main>
+        )
 }
-
-export default RankItems
-
+export default RankItems;
 
--- MovieRating-App/MovieRating-App/ClientApp/src/components/RankingGrid.js
+++ MovieRating-App/MovieRating-App/ClientApp/src/components/RankingGrid.js
@@ -1,6 +1,4 @@
-import React from 'react'
-
-const RankingGrid = ({items, imgArr}) => {
+const RankingGrid = ({items, imgArr, drag, allowDrop, drop }) => {
 
     const rankingGrid = [];
     const cellCollectionTop = [];
@@ -9,71 +7,111 @@ const RankingGrid = ({items, imgArr}) => {
     const cellCollectionWorst = [];
 
     function pushCellMarkupToArr(cellCollection, rankNum, rowLabel) {
-        if(rankNum > 0) {
-            let item = items.find(o => o.ranking === rankNum);
-            cellCollection.push(<div id={`rank-${rankNum}`} className='rank-cell'></div>)
+        if (rankNum > 0) {
+            var item = items.find(o => o.ranking === rankNum);
+            cellCollection.push(
+                <div
+                    key={`cell-${rankNum}`} // Add a unique key prop here
+                    id={`rank-${rankNum}`}
+                    onDrop={drop}
+                    onDragOver={allowDrop}
+                    className="rank-cell"
+                >
+                    {(item != null) ? (
+                        <img
+                            key={`img-${item.id}`} // Add a unique key prop here
+                            id={`item-${item.id}`}
+                            src={imgArr.find(o => o.id === item.imageId)?.image}
+                            draggable="true"
+                            onDragStart={drag}
+                        />
+                    ) : null}
+                </div>
+            );
         } else {
-            cellCollection.push(<div className='row-label'>
-                <h4>{rowLabel}</h4>
-            </div>)
+            cellCollection.push(
+                <div key={`label-${rowLabel}`} className="row-label">
+                    <h4>{rowLabel}</h4>
+                </div>
+            );
         }
     }
 
     function createCellsForRow(rowNum) {
-        let rankNum = 0;
-        let currCollection = [];
-        let label = "";
+        var rankNum = 0;
+        var currCollection = [];
+        var label = "";
         const numCells = 5;
 
-        for (let a = 1; a <= numCells; a++) {
+        for (var a = 1; a <= numCells; a++) {
             rankNum = (a === 1) ? 0 : (numCells * (rowNum - 1)) + a - rowNum;
 
-            if(rowNum === 1) {
+            if (rowNum === 1) {
                 currCollection = cellCollectionTop;
-                label = "Tor Tier";
-            } else if (rowNum === 2) {
+                label = "Top Tier";
+            }
+            else if (rowNum === 2) {
                 currCollection = cellCollectionMiddle;
                 label = "Middle Tier";
-            } else if (rowNum === 3) {
+            }
+            else if (rowNum === 3) {
                 currCollection = cellCollectionBottom;
                 label = "Bottom Tier";
-            } else if (rowNum === 4) {
+            }
+            else if (rowNum === 4) {
                 currCollection = cellCollectionWorst;
-                label = "Worst Tier"
+                label = "Worst Tier";
             }
             pushCellMarkupToArr(currCollection, rankNum, label);
 
         }
-    }
 
+    }
 
     function createCellsForRows() {
         const maxRows = 4;
-        for (let row = 1; row <= maxRows; row++){
-            createCellsForRow(row)
+        for (var row = 1; row <= maxRows; row++) {
+            createCellsForRow(row);
         }
     }
 
     function createRowsForGrid() {
-        rankingGrid.push(<div className='rank-row top-tier'>{cellCollectionTop}</div>);
-        rankingGrid.push(<div className='rank-row middle-tier'>{cellCollectionMiddle}</div>);
-        rankingGrid.push(<div className='rank-row bottom-tier'>{cellCollectionBottom}</div>);
-        rankingGrid.push(<div className='rank-row worst-tier'>{cellCollectionWorst}</div>);
-
+        rankingGrid.push(
+            <div key={items.id} className="rank-row top-tier">
+                {cellCollectionTop}
+            </div>
+        );
+        rankingGrid.push(
+            <div key="middle-tier" className="rank-row middle-tier">
+                {cellCollectionMiddle}
+            </div>
+        );
+        rankingGrid.push(
+            <div key="bottom-tier" className="rank-row bottom-tier">
+                {cellCollectionBottom}
+            </div>
+        );
+        rankingGrid.push(
+            <div key="worst-tier" className="rank-row worst-tier">
+                {cellCollectionWorst}
+            </div>
+        );
+    
         return rankingGrid;
     }
+    
 
     function createRankingGrid() {
         createCellsForRows();
         return createRowsForGrid();
-
     }
 
     return (
-    <div className='rankings'>
-        {createRankingGrid()}
-    </div>
+        <div className="rankings">
+            {createRankingGrid()}
+        </div>
+            
     )
-}
 
-export default RankingGrid
+}
+export default RankingGrid;
